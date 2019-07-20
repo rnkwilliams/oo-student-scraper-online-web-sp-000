e5227565486a33c5a6feaafd82ee3a210a7dcaa1
Ranika Williams <rnkprice07@gmail.com>
2019-07-20 22:58:00 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,71 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+
+    student_names = doc.css(".student-name")
+    student_names_array = []
+    student_names.each do |name|
+      student_names_array << name.text
+    end
+
+    student_locations = doc.css(".student-location")
+    student_locations_array = []
+    student_locations.each do |location|
+      student_locations_array << location.text
+    end
+
+    student_urls = doc.css(".student-card a[href]")
+    student_urls_array = []
+    student_urls.each do |profile_url|
+      student_urls_array << profile_url["href"]
+    end
+
+    combined_array = []
+    x = 0
+    student_names_array.each do |name|
+      combined_array << {:name => name, :location => student_locations_array[x], :profile_url => student_urls_array[x]}
+      x += 1
+    end
+    combined_array
   end
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+    html = open (profile_url)
+    doc = Nokogiri::HTML(html)
 
+    student_profile_hash = {}
+
+    social_links = doc.css(".social-icon-container a[href]")
+      social_links.each do |link|
+        if link["href"].include?("twitter")
+        student_profile_hash[:twitter]= link["href"]
+
+        elsif link["href"].include?("linkedin")
+        student_profile_hash[:linkedin]= link["href"]
+
+
+        elsif link["href"].include?("github")
+          student_profile_hash[:github]= link["href"]
+
+        elsif !(link["href"].include?("github")) &&
+           !(link["href"].include?("linkedin")) &&
+           !(link["href"].include?("twitter")) &&
+           !(link["href"].include?("facebook")) &&
+           !(link["href"].include?("youtube"))
+          student_profile_hash[:blog]= link["href"]
+        end
+      end
+
+
+      student_profile_hash[:profile_quote] = doc.css(".profile-quote").text
+      student_profile_hash[:bio] = doc.css(".description-holder p").text
+      student_profile_hash
+
+    end
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,37 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
+  #The #new  or initialize method takes in an argument of a hash and sets
+  #that new student's attributes using the key/value pairs of that hash.
+  #It also adds that new student to the Student class' collection of all
+  #existing students, stored in the `@@all` class variable.
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    @@all << self
   end
 
+  #uses the Scraper class to create new students with the correct name and location.
   def self.create_from_collection(students_array)
-    
+    students_array.each do |individual|
+     @@all << Student.new(individual)
+    end
   end
 
+  #uses the Scraper class to get a hash of a given students attributes
+  #and uses that hash to set additional attributes for that student.
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key,value|
+      self.send("#{key}=", value)
+    end
   end
 
+  #returns the class variable @@all
   def self.all
-    
+    @@all
   end
 end
-
