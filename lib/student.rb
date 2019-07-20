aeb943fa6a58719a0d5d96302f78a409627604f4
Ranika Williams <rnkprice07@gmail.com>
2019-07-20 22:52:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,15 +15,23 @@ class Student
     @@all << self
   end
 
+  #uses the Scraper class to create new students with the correct name and location.
   def self.create_from_collection(students_array)
-
+    students_array.each do |individual|
+     @@all << Student.new(individual)
+    end
   end
 
+  #uses the Scraper class to get a hash of a given students attributes
+  #and uses that hash to set additional attributes for that student.
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each do |key,value|
+      self.send("#{key}=", value)
+    end
   end
 
+  #returns the class variable @@all
   def self.all
-
+    @@all
   end
 end
